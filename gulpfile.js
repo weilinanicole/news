dc1d516ed5e166cc3fc7fe2d1c72ed6d0fa94009
wln <503935904@qq.com>
2018-04-15 23:33:25 +0800

    end

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var data = require('./src/data/data.json');
 var cleanCss = require('gulp-clean-css');
-var scss = rrequire('gulp-sass');
+var scss = require('gulp-sass');
 var minHtml = require('gulp-htmlmin');
 var minImg = require('gulp-imagemin');
 var sequence = require('gulp-sequence');
@@ -12,9 +12,19 @@ gulp.task('minCss', function() {
         .pipe(cleanCss())
         .pipe(gulp.dest('dist/css'))
 });
+var options = {
+    removeComments: true, //清除HTML注释
+    collapseWhitespace: true, //压缩HTML
+    collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+    removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+    removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+    minifyJS: true, //压缩页面JS
+    minifyCSS: true //压缩页面CSS
+}
 gulp.task('minHtml', function() {
     gulp.src('src/*.html')
-        .pipe(minHtml())
+        .pipe(minHtml(options))
         .pipe(gulp.dest('dist'))
 });
 gulp.task('minImg', function() {
@@ -29,11 +39,11 @@ gulp.task('server', function() {
             open: true,
             livereload: true,
             middleware: function(req, res, next) {
-                console.log(req, url)
+                if (/\/login/g.test(req.url)) {
+                    res.end(JSON.stringify(data))
+                }
                 next()
             }
         }))
 })
-gulp.task('default', function() {
-    sequence(['minCss', 'minHtml', 'minImg'])
-})
+gulp.task('default', ['minCss', 'minHtml', 'minImg', 'server'])
